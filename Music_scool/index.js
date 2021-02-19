d62edcfb2c62faaab13605f06701826a43ae065d
Duccaa <gojicduca@yahoo.com>
2021-02-20 00:50:13 +0100

    responsive navbar update

--- Music_scool/index.js
+++ Music_scool/index.js
@@ -1,11 +1,21 @@
 const list = document.getElementById('list');
 const button = document.getElementById('button');
+const i = document.getElementById('toggle');
  
 button.addEventListener('click', () => {
+
     list.classList.contains("block") 
     ? 
     list.classList.remove("block") 
     : 
     list.classList.add("block");
-});
 
+    if(i.classList.contains("fa-bars")) {
+        i.classList.remove("fa-bars");
+        i.classList.add("fa-times")
+    } else {
+        i.classList.remove("fa-times");
+        i.classList.add("fa-bars");
+    }
+
+});
